6c58b229e78353d7f0bd3567cca7906e70861b33
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2024-09-23 10:56:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -942,7 +942,7 @@ def notification():
     return render_template('notification.html')
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Corrected method to create all tables
-    uvicorn.run('app:app', host='0.0.0.0', port=8000)
+#if __name__ == "__main__":
+    #with app.app_context():
+        #db.create_all()  # Corrected method to create all tables
+    #uvicorn.run('app:app', host='0.0.0.0', port=8000)
