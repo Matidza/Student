8d989ebc2f8d67a647a0990c2c9b2260bb4c18db
Matidza <Matidza46@gmail.com>
2024-10-01 14:13:35 +0200

    Nginx Web Server set-up

--- app.py
+++ app.py
@@ -943,6 +943,5 @@ def notification():
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Corrected method to create all tables
-    app.run( host='0.0.0.0', port=8000)
+    
+    app.run()
